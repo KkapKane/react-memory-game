c1b282614e18872be5a487a002564379bbf358d3
Kane <LisaxKane@gmail.com>
2022-12-12 21:55:03 -0600

    added favicon and updated readme

--- README.md
+++ README.md
@@ -1,3 +1,63 @@
-# react-memory-game
 
-live preview : https://kkapkane.github.io/react-memory-game
+
+<h1 align="center">:maple_leaf: React-Memory-Game :maple_leaf:</h1>  
+
+
+<h2> <h2>
+
+Click [here](https://kkapkane.github.io/react-memory-game) for live demo.
+# Made with
+
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![SASS](https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white)
+![Visual Studio Code](https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)
+<br>
+<br>
+
+
+
+# Description
+
+<h4> A game that test your memorization skills! </h4>
+
+<h2> </h2>
+
+# Instructions
+<p>Start the game by picking any card. Continue picking cards that hasn't been clicked yet. Getting a score of 12 means you won the game!</p>
+
+
+# Features
+- Shuffles card after each pick
+- Clicking the same card twice will invoke the 'you lose' alert
+- Beating the game will reward you with confetti
+
+
+
+# Previews
+
+
+
+<img src="https://s9.gifyu.com/images/memoryGame.png" alt="preview" width="900">
+
+
+
+# Things I learned from this Project
+
+* There's probably a library out there that does exactly what you need
+```bash
+npm install react-confetti
+```
+* Discovered what ternary operators are on this project
+```js
+win ? <Confetti /> : null
+```
+* Dabbled more with useEffect and used it to check if player won
+```js
+useEffect(() => {
+    if (picked.length >= 12) {
+      alert("you won!");
+      setWin(true);
+    }
+  }, [picked]);
+```
--- src/App.js
+++ src/App.js
@@ -1,96 +1,123 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./component/Card";
-import { v4 as uuidv4 } from 'uuid';
-import Confetti from 'react-confetti'
-
-
+import { v4 as uuidv4 } from "uuid";
+import Confetti from "react-confetti";
 
 function App() {
-
   function shuffle(array) {
-    let currentIndex = array.length, randomIndex;
+    let currentIndex = array.length,
+      randomIndex;
 
     // While there remain elements to shuffle.
     while (currentIndex !== 0) {
-
       // Pick a remaining element.
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
 
       // And swap it with the current element.
       [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+        array[randomIndex],
+        array[currentIndex],
+      ];
     }
 
     return array;
   }
 
+  const mob1 =
+    "https://cdn.wikimg.net/en/strategywiki/images/7/73/MS_Monster_Big_Boulder_Muncher.png";
+  const mob2 =
+    "https://cdn.wikimg.net/en/strategywiki/images/6/6e/MS_Monster_Squirrelnon.png";
+  const mob3 =
+    "https://cdn.wikimg.net/en/strategywiki/images/f/f7/MS_Monster_Blue_Mushmom.png";
+  const mob4 =
+    "https://cdn.wikimg.net/en/strategywiki/images/c/ca/MS_Monster_Buffoon.png";
+  const mob5 =
+    "https://cdn.wikimg.net/en/strategywiki/images/6/69/MS_Monster_Chimera.png";
+  const mob6 =
+    "https://cdn.wikimg.net/en/strategywiki/images/7/74/MS_Monster_Furious_Scarlion_Boss.png";
+  const mob7 =
+    "https://cdn.wikimg.net/en/strategywiki/images/1/1c/MS_Monster_Heretic_Monk_Initiate.png";
+  const mob8 =
+    "https://cdn.wikimg.net/en/strategywiki/images/c/cc/MS_Monster_Mossy_Snail.png";
+  const mob9 =
+    "https://cdn.wikimg.net/en/strategywiki/images/d/d6/MS_Monster_Mutant_Ribbon_Pig.png";
+  const mob10 =
+    "https://cdn.wikimg.net/en/strategywiki/images/a/a8/MS_Monster_Prison_Guard_Ani.png";
+  const mob11 =
+    "https://cdn.wikimg.net/en/strategywiki/images/2/20/MS_Monster_Sinister_Wooden_Mask.png";
+  const mob12 =
+    "https://cdn.wikimg.net/en/strategywiki/images/e/e4/MS_Monster_Snowman.png";
 
+  const [card, setCard] = useState([
+    mob2,
+    mob1,
+    mob3,
+    mob4,
+    mob5,
+    mob6,
+    mob7,
+    mob8,
+    mob9,
+    mob10,
+    mob11,
+    mob12,
+  ]);
 
+  const [picked, setPicked] = useState([]);
 
-  const mob1 = "https://cdn.wikimg.net/en/strategywiki/images/7/73/MS_Monster_Big_Boulder_Muncher.png"
-  const mob2 = "https://cdn.wikimg.net/en/strategywiki/images/6/6e/MS_Monster_Squirrelnon.png"
-  const mob3 = "https://cdn.wikimg.net/en/strategywiki/images/f/f7/MS_Monster_Blue_Mushmom.png"
-  const mob4 = "https://cdn.wikimg.net/en/strategywiki/images/c/ca/MS_Monster_Buffoon.png"
-  const mob5 = "https://cdn.wikimg.net/en/strategywiki/images/6/69/MS_Monster_Chimera.png"
-  const mob6 = "https://cdn.wikimg.net/en/strategywiki/images/7/74/MS_Monster_Furious_Scarlion_Boss.png"
-  const mob7 = "https://cdn.wikimg.net/en/strategywiki/images/1/1c/MS_Monster_Heretic_Monk_Initiate.png"
-  const mob8 = "https://cdn.wikimg.net/en/strategywiki/images/c/cc/MS_Monster_Mossy_Snail.png"
-  const mob9 = "https://cdn.wikimg.net/en/strategywiki/images/d/d6/MS_Monster_Mutant_Ribbon_Pig.png"
-  const mob10 = "https://cdn.wikimg.net/en/strategywiki/images/a/a8/MS_Monster_Prison_Guard_Ani.png"
-  const mob11 = "https://cdn.wikimg.net/en/strategywiki/images/2/20/MS_Monster_Sinister_Wooden_Mask.png"
-  const mob12 = "https://cdn.wikimg.net/en/strategywiki/images/e/e4/MS_Monster_Snowman.png"
+  const [highscore, setHighscore] = useState(0);
 
-  const [card, setCard] = useState([mob2, mob1, mob3, mob4, mob5, mob6, mob7, mob8, mob9, mob10, mob11, mob12])
-  
-  const [picked, setPicked] = useState([])
-
-  const [highscore, setHighscore] = useState(0)
-
-  const [win, setWin] = useState(false)
+  const [win, setWin] = useState(false);
 
   const handleClick = (s) => {
-    if (picked.length >= 12) {
-      alert("you won!")
-      setWin(true)
-    }
     if (picked.includes(s)) {
-      alert('you lose')
+      alert("you lose");
 
       if (picked.length > highscore) {
-        setHighscore(picked.length)
+        setHighscore(picked.length);
       }
 
-      setPicked([])
-    }
-    else
-      setPicked(current => [...picked, s])
+      setPicked([]);
+    } else setPicked((current) => [...picked, s]);
     shuffle(card);
+  };
 
-  }
+  useEffect(() => {
+    if (picked.length >= 12) {
+      alert("you won!");
+      setWin(true);
+    }
+  }, [picked]);
 
   return (
-
-    <div className="App">
+    <div className='App'>
       {win ? <Confetti /> : null}
 
-      <div className="header">
-
-        <img src="https://upload.wikimedia.org/wikipedia/en/3/37/MapleStory.SVG" alt="" className="logo" />
-        <div className="scoreContainer">
-          <div className="scoreBoard">score: {picked.length}</div>
-          <div className="highScore">high score: {highscore}</div>
+      <div className='header'>
+        <img
+          src='https://upload.wikimedia.org/wikipedia/en/3/37/MapleStory.SVG'
+          alt=''
+          className='logo'
+        />
+        <div className='scoreContainer'>
+          <div className='scoreBoard'>Score: {picked.length}</div>
+          <div className='highScore'>High Score: {highscore}</div>
         </div>
       </div>
 
-      <div className="cardContainer">
+      <div className='cardContainer'>
         {card.map((c) => {
-          return (<Card key={uuidv4()} picked={picked} card={c} handleClick={handleClick} />)
+          return (
+            <Card
+              key={uuidv4()}
+              picked={picked}
+              card={c}
+              handleClick={handleClick}
+            />
+          );
         })}
       </div>
-
-
-
     </div>
   );
 }
